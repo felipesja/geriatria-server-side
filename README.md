dc9cbfeb6f8794c0b8df1ddfc8da4b8178f33037
felipesja <felipesantos.uerj@gmail.com>
2019-08-05 13:39:33 -0300

    Upload do Projeto

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# geriatria-server-side
+# Geriatria Coração de Mária 
+This project is a website(server side) from a geriatric clinic called "Lar Geriátrico Coração de Mária".
+
+## Build With 
+This is a server side firebase function developed in Node.js.
+
+Hosted in [Firebase Funtions](https://firebase.google.com/docs/functions).
+
+## Getting Started
+The password and e-mails are encapsulated in Custom Environment Variables.
+
+For more details: [NPM dotenv](https://www.npmjs.com/package/dotenv).   
---
+++ functions/index.js
@@ -0,0 +1,50 @@
+'use strict';
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
+
+require('dotenv').config();
+
+let transporter = nodemailer.createTransport({
+  service: 'Gmail', // no need to set host or port etc.
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASSWORD
+  }
+});
+
+exports.enviarEmail = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+
+    const { email = '', name = '', telefone = '', message = '' } = req.body;
+
+    const from = name && email ? `${name} <${email}>` : `${name || email}`;
+
+    let htmlBody = `<b>Informações do Contato</b><br><br>
+                    <b>Nome: </b>${name}<br>
+                    <b>Telefone: </b>${telefone}<br>
+                    <b>E-mail: </b>${email}<br><br>
+                    <b>Mensagem: </b><br>
+                    <p>${message}</p>`;
+
+    const messageEmail = {
+      from,
+      to: process.env.TO,
+      subject: `Novo contato de ${from} realizado através do site`,
+      message,
+      replyTo: from,
+      html: htmlBody
+    };
+
+    transporter.sendMail(messageEmail, (error, info) => {
+      if (error) {
+        console.log(`Failed to send the message "${message}" from <${name}> ${email} with the error ${error && error.message}`);
+        res.status(500).send("Error");
+      } else {
+        console.log(`Sent the message "${message}" from <${name}> ${email}.`);
+        console.log(info.envelope);
+        res.status(200).send("Sucess");
+      }
+    });
+  });
+});
